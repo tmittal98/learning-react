84deb1729bd4e9c01d65a5a5b0c37aae9a1f7b0d
Tushar <tusharmittal429@gmail.com>
2021-03-03 17:04:05 +0530

    tutorial on JSX

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import About from './components/About'
 import Contact from './components/Contact'
 
 function App() {
+  //JSX Tutorial (Javascript XML)
+  //Writing JS in HTML 
+  //Allows to write JS and HTML togrther
+  let data = "contact us component"
+
   return (
     <div>
-      <h1>Hello</h1> 
+      <h1>{data}</h1> 
        <About />
        <Contact />
     </div>
